d404c79f323f0572b90912a8da1a7bd170d525cd
Lovecraft255 <dilanbemitez6@gmail.com>
2024-06-07 21:41:12 -0300

    Sistema de imagenes mejoradas

--- App_Client_games/src/components/Form.jsx
+++ App_Client_games/src/components/Form.jsx
@@ -1,167 +1,223 @@
-import React, { useState } from 'react';
-
-
+import React, { useState } from "react";
 
 const Form = ({ setGames }) => {
+  const [caract, setCaract] = useState({
+    cant: "",
+    dev: "",
+    gen: "",
+    plat: "",
+    eng: "",
+    modes: "",
+    minDate: "1970-12-31",
+    maxDate: "2023-12-31",
+    order: "",
+  });
 
-    const [caract, setCaract] = useState({
-        cant: "",
-        dev: "",
-        gen: "",
-        plat: "",
-        eng: "",
-        modes: "",
-        minDate: "1970-12-31",
-        maxDate: "2023-12-31",
-        order: ""
+  const pedirDatos = async (e) => {
+    e.preventDefault();
+    let url = "https://games-app-back-rjnk.onrender.com/games";
 
-    });
-
-
-    const pedirDatos = async (e) => {
-        e.preventDefault();
-        let url = 'https://games-app-back-rjnk.onrender.com/games';
-
-        if (caract.cant || caract.dev || caract.gen || caract.minDate || caract.plat || caract.eng || caract.maxDate || caract.order) {
-            url += "?";
-            if (caract.cant) url += 'cant=' + caract.cant + '&';
-            if (caract.dev) url += 'developer=' + caract.dev + '&';
-            if (caract.gen) url += 'genre=' + caract.gen + '&';
-            if (caract.plat) url += 'plataform=' + caract.plat + '&';
-            if (caract.eng) url += 'engine=' + caract.plat + '&';
-            if (caract.minDate) url += 'minDate=' + caract.minDate + '&';
-            if (caract.maxDate) url += 'maxDate=' + caract.maxDate + '&';
-            if (caract.order) url += 'order=' + caract.order + '&';
-        }
-
-        const req = fetch(url);
-        req.then((res) => res.json())
-            .then(game => {
-                console.log(game);
-                setGames(game.games)
-            })
-
+    if (
+      caract.cant ||
+      caract.dev ||
+      caract.gen ||
+      caract.minDate ||
+      caract.plat ||
+      caract.eng ||
+      caract.maxDate ||
+      caract.order
+    ) {
+      url += "?";
+      if (caract.cant) url += "cant=" + caract.cant + "&";
+      if (caract.dev) url += "developer=" + caract.dev + "&";
+      if (caract.gen) url += "genre=" + caract.gen + "&";
+      if (caract.plat) url += "plataform=" + caract.plat + "&";
+      if (caract.eng) url += "engine=" + caract.plat + "&";
+      if (caract.minDate) url += "minDate=" + caract.minDate + "&";
+      if (caract.maxDate) url += "maxDate=" + caract.maxDate + "&";
+      if (caract.order) url += "order=" + caract.order + "&";
     }
 
-    return (
-        <><div></div>
-            <form style={{ border: '4px solid black', fontSize: '20px', color: '#000000', padding: '15px', backgroundColor: '#FFC0CB', width: '40%'}} onSubmit={pedirDatos} >
-                <label>Cantidad de juegos que desea</label>
-                <input style={{ marginLeft: '10px', marginBottom: '10px' }} id="cant" value={caract.cant} type="number" min="1" max="7"
-                    onChange={e => {
-                        setCaract(prev => {
-                            return {
-                                ...prev,
-                                [e.target.id]: e.target.value
-                            };
-                        });
-                    }} />
+    const req = fetch(url);
+    req
+      .then((res) => res.json())
+      .then((game) => {
+        console.log(game);
+        setGames(game.games);
+      });
+  };
 
-                <br />
+  return (
+    <>
+      <div></div>
+      <form
+        style={{
+          border: "4px solid black",
+          fontSize: "20px",
+          color: "#000000",
+          padding: "15px",
+          backgroundColor: "#FFC0CB",
+          width: "40%",
+        }}
+        onSubmit={pedirDatos}
+      >
+        <label>Cantidad de juegos que desea</label>
+        <input
+          style={{ marginLeft: "10px", marginBottom: "10px" }}
+          id="cant"
+          value={caract.cant}
+          type="number"
+          min="1"
+          max="7"
+          onChange={(e) => {
+            setCaract((prev) => {
+              return {
+                ...prev,
+                [e.target.id]: e.target.value,
+              };
+            });
+          }}
+        />
 
-                <label>Empresas de videojuegos que desea</label>
-                <input style={{ marginLeft: '10px', marginBottom: '10px' }} id="dev" value={caract.dev} type="text" placeholder="none"
-                    onChange={e => {
-                        setCaract(prev => {
-                            return {
-                                ...prev,
-                                [e.target.id]: e.target.value
-                            };
-                        });
-                    }} />
+        <br />
 
-                <br />
+        <label>Empresas de videojuegos que desea</label>
+        <input
+          style={{ marginLeft: "10px", marginBottom: "10px" }}
+          id="dev"
+          value={caract.dev}
+          type="text"
+          placeholder="none"
+          onChange={(e) => {
+            setCaract((prev) => {
+              return {
+                ...prev,
+                [e.target.id]: e.target.value,
+              };
+            });
+          }}
+        />
 
-                <label>Genero de videojuegos que desea</label>
-                <input style={{ marginLeft: '10px', marginBottom: '10px' }} id="gen" value={caract.gen} type="text" placeholder="none"
-                    onChange={e => {
-                        setCaract(prev => {
-                            return {
-                                ...prev,
-                                [e.target.id]: e.target.value
-                            };
-                        });
-                    }} />
+        <br />
 
-                <br />
+        <label>Genero de videojuegos que desea</label>
+        <input
+          style={{ marginLeft: "10px", marginBottom: "10px" }}
+          id="gen"
+          value={caract.gen}
+          type="text"
+          placeholder="none"
+          onChange={(e) => {
+            setCaract((prev) => {
+              return {
+                ...prev,
+                [e.target.id]: e.target.value,
+              };
+            });
+          }}
+        />
 
-                <label>Plataforma de videojuegos que desea</label>
-                <input style={{ marginLeft: '10px', marginBottom: '10px' }} id="plat" value={caract.plat} type="text" placeholder="none"
-                    onChange={e => {
-                        setCaract(prev => {
-                            return {
-                                ...prev,
-                                [e.target.id]: e.target.value
-                            };
-                        });
-                    }} />
+        <br />
 
-                <br />
-                <label>Fecha minima para los juegos lanzados</label>
-                <input style={{ marginLeft: '10px', marginBottom: '10px' }} id="minDate" value={caract.minDate} type="date"
-                    onChange={e => {
-                        setCaract(prev => {
-                            return {
-                                ...prev,
-                                [e.target.id]: e.target.value
-                            };
-                        });
-                        console.log(e.target.value);
-                    }} />
+        <label>Plataforma de videojuegos que desea</label>
+        <input
+          style={{ marginLeft: "10px", marginBottom: "10px" }}
+          id="plat"
+          value={caract.plat}
+          type="text"
+          placeholder="none"
+          onChange={(e) => {
+            setCaract((prev) => {
+              return {
+                ...prev,
+                [e.target.id]: e.target.value,
+              };
+            });
+          }}
+        />
 
-                <br />
-                <label>Fecha maxima para los juegos lanzados</label>
-                <input style={{ marginLeft: '10px', marginBottom: '10px' }} id="maxDate" value={caract.maxDate} type="date"
-                    onChange={e => {
-                        setCaract(prev => {
-                            return {
-                                ...prev,
-                                [e.target.id]: e.target.value
-                            };
-                        });
-                    }} />
+        <br />
+        <label>Fecha minima para los juegos lanzados</label>
+        <input
+          style={{ marginLeft: "10px", marginBottom: "10px" }}
+          id="minDate"
+          value={caract.minDate}
+          type="date"
+          onChange={(e) => {
+            setCaract((prev) => {
+              return {
+                ...prev,
+                [e.target.id]: e.target.value,
+              };
+            });
+            console.log(e.target.value);
+          }}
+        />
 
-                <br />
-                <label>Que tipo de ordenamiento quiere</label>
-                <select style={{ marginLeft: '10px', marginBottom: '10px' }} name="Orden" id="order" value={caract.order}
-                    onChange={e => {
-                        setCaract(prev => {
-                            return {
-                                ...prev,
-                                [e.target.id]: e.target.value
-                            };
-                        });
-                    }}
-                >
-                    <option value="default">ninguno</option>
-                    <option value="title">title</option>
-                    <option value="date">date</option>
-                </select>
-                <br />
-                <input
-                    style={{
-                        border: '4px solid black',
-                        fontSize: '20px',
-                        marginTop: '10px',
-                        width: '50%',
-                        marginLeft: '140px',
-                        marginRight: 'auto',
-                        transition: 'background-color 0.3s ease', /* Añadimos una transición para suavizar el cambio */
-                    }}
-                    type="submit"
-                    value="Solicitar juegos"
-                    onMouseOver={e => e.target.style.backgroundColor = '#CCCCCC'} /* Cambia el color de fondo al pasar el mouse sobre el elemento */
-                    onMouseOut={e => e.target.style.backgroundColor = 'white'} /* Vuelve a cambiar el color de fondo cuando el mouse deja el elemento */
-                />
+        <br />
+        <label>Fecha maxima para los juegos lanzados</label>
+        <input
+          style={{ marginLeft: "10px", marginBottom: "10px" }}
+          id="maxDate"
+          value={caract.maxDate}
+          type="date"
+          onChange={(e) => {
+            setCaract((prev) => {
+              return {
+                ...prev,
+                [e.target.id]: e.target.value,
+              };
+            });
+          }}
+        />
 
-                <br />
-                <br />
-            </form></>
+        <br />
+        <label>Que tipo de ordenamiento quiere</label>
+        <select
+          style={{ marginLeft: "10px", marginBottom: "10px" }}
+          name="Orden"
+          id="order"
+          value={caract.order}
+          onChange={(e) => {
+            setCaract((prev) => {
+              return {
+                ...prev,
+                [e.target.id]: e.target.value,
+              };
+            });
+          }}
+        >
+          <option value="default">ninguno</option>
+          <option value="title">title</option>
+          <option value="date">date</option>
+        </select>
+        <br />
+        <input
+          style={{
+            border: "4px solid black",
+            fontSize: "20px",
+            marginTop: "10px",
+            width: "50%",
+            marginLeft: "140px",
+            marginRight: "auto",
+            transition:
+              "background-color 0.3s ease" /* Añadimos una transición para suavizar el cambio */,
+          }}
+          type="submit"
+          value="Solicitar juegos"
+          onMouseOver={(e) =>
+            (e.target.style.backgroundColor = "#CCCCCC")
+          } /* Cambia el color de fondo al pasar el mouse sobre el elemento */
+          onMouseOut={(e) =>
+            (e.target.style.backgroundColor = "white")
+          } /* Vuelve a cambiar el color de fondo cuando el mouse deja el elemento */
+        />
 
-
-
-    )
-}
+        <br />
+        <br />
+      </form>
+    </>
+  );
+};
 
 export default Form;
-
--- App_Client_games/src/components/Juego.jsx
+++ App_Client_games/src/components/Juego.jsx
@@ -1,33 +1,148 @@
-import React from 'react';
+import React from "react";
 
 const Juego = ({ game }) => {
+  console.log(game);
 
-    console.log(game);
-
-    function DefinirModosDeJuegos() {
-        if (game.modes.single_player === true && game.modes.multiplayer === true) {
-            return "Single player and multiplayer";
-        } else if (game.modes.single_player === true && game.modes.multiplayer === false) {
-            return "Only single player";
-        } else if (game.modes.single_player === false && game.modes.multiplayer === true) {
-            return "Only multiplayer";
-        }
+  function DefinirModosDeJuegos() {
+    if (game.modes.single_player === true && game.modes.multiplayer === true) {
+      return "Single player and multiplayer";
+    } else if (
+      game.modes.single_player === true &&
+      game.modes.multiplayer === false
+    ) {
+      return "Only single player";
+    } else if (
+      game.modes.single_player === false &&
+      game.modes.multiplayer === true
+    ) {
+      return "Only multiplayer";
     }
+  }
 
-    return (
-        <tr style={{ border: '4px solid black', width: '80%' }}>
-            <td style={{ width: '80%', fontSize: '100%', borderRight: "4px solid black", borderBottom: "4px solid black", textAlign: 'center' }}>{game.title}</td>
-            <td style={{ width: '80%', fontSize: '100%', borderRight: "4px solid black", borderBottom: "4px solid black", textAlign: 'center' }}>{game.developer}</td>
-            <td style={{ width: '80%', fontSize: '100%', borderRight: "4px solid black", borderBottom: "4px solid black", textAlign: 'center' }}>{game.publisher}</td>
-            <td style={{ width: '80%', fontSize: '100%', borderRight: "4px solid black", borderBottom: "4px solid black", textAlign: 'center' }}>{game.genres}</td>
-            <td style={{ width: '80%', fontSize: '100%', borderRight: "4px solid black", borderBottom: "4px solid black", textAlign: 'center' }}>{game.release.day}/{game.release.month}/{game.release.year}</td>
-            <td style={{ width: '80%', fontSize: '100%', borderRight: "4px solid black", borderBottom: "4px solid black", textAlign: 'center' }}>{game.platforms.map(plat => plat.name + ",")}</td>
-            <td style={{ width: '80%', fontSize: '100%', borderRight: "4px solid black", borderBottom: "4px solid black", textAlign: 'center' }}>{game.engine}</td>
-            <td style={{ width: '80%', fontSize: '100%', borderRight: "4px solid black", borderBottom: "4px solid black", textAlign: 'center' }}>{DefinirModosDeJuegos()}</td>
-            <td style={{ overflowY: 'auto', width: '80%', borderRight: "4px solid black", borderBottom: "4px solid black", textAlign: 'center' }}>{game.images.map(g => <img style={{ width: '100px', height: '100px'}} key={g} src={g} alt="Game Image" ></img>)}</td>
-            <td style={{ overflowY: 'scroll', height: '420px', float: 'right', width: '80%', fontSize: '100%', borderRight: "4px solid black", borderBottom: "4px solid black", textAlign: 'center' }}>{game.premise}</td>
-        </tr>
-    );
+  return (
+    <tr style={{ border: "4px solid black", width: "80%" }}>
+      <td
+        style={{
+          width: "80%",
+          fontSize: "100%",
+          borderRight: "4px solid black",
+          borderBottom: "4px solid black",
+          textAlign: "center",
+        }}
+      >
+        {game.title}
+      </td>
+      <td
+        style={{
+          width: "80%",
+          fontSize: "100%",
+          borderRight: "4px solid black",
+          borderBottom: "4px solid black",
+          textAlign: "center",
+        }}
+      >
+        {game.developer}
+      </td>
+      <td
+        style={{
+          width: "80%",
+          fontSize: "100%",
+          borderRight: "4px solid black",
+          borderBottom: "4px solid black",
+          textAlign: "center",
+        }}
+      >
+        {game.publisher}
+      </td>
+      <td
+        style={{
+          width: "80%",
+          fontSize: "100%",
+          borderRight: "4px solid black",
+          borderBottom: "4px solid black",
+          textAlign: "center",
+        }}
+      >
+        {game.genres}
+      </td>
+      <td
+        style={{
+          width: "80%",
+          fontSize: "100%",
+          borderRight: "4px solid black",
+          borderBottom: "4px solid black",
+          textAlign: "center",
+        }}
+      >
+        {game.release.day}/{game.release.month}/{game.release.year}
+      </td>
+      <td
+        style={{
+          width: "80%",
+          fontSize: "100%",
+          borderRight: "4px solid black",
+          borderBottom: "4px solid black",
+          textAlign: "center",
+        }}
+      >
+        {game.platforms.map((plat) => plat.name + ",")}
+      </td>
+      <td
+        style={{
+          width: "80%",
+          fontSize: "100%",
+          borderRight: "4px solid black",
+          borderBottom: "4px solid black",
+          textAlign: "center",
+        }}
+      >
+        {game.engine}
+      </td>
+      <td
+        style={{
+          width: "80%",
+          fontSize: "100%",
+          borderRight: "4px solid black",
+          borderBottom: "4px solid black",
+          textAlign: "center",
+        }}
+      >
+        {DefinirModosDeJuegos()}
+      </td>
+      <td
+        style={{
+          overflowY: "auto",
+          width: "80%",
+          borderRight: "4px solid black",
+          borderBottom: "4px solid black",
+          textAlign: "center",
+        }}
+      >
+        {game.images.map((IUrl) => (
+          <img
+            style={{ width: "100px", height: "100px" }}
+            key={IUrl}
+            src={"http://localhost:5173/"+ IUrl}
+            alt="Game Image"
+          ></img>
+        ))}
+      </td>
+      <td
+        style={{
+          overflowY: "scroll",
+          height: "420px",
+          float: "right",
+          width: "80%",
+          fontSize: "100%",
+          borderRight: "4px solid black",
+          borderBottom: "4px solid black",
+          textAlign: "center",
+        }}
+      >
+        {game.premise}
+      </td>
+    </tr>
+  );
 };
 
 export default Juego;
